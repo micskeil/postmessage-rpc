c94b25d8f448c00e035a46c9548e4d34892dae77
Lali Micskei <micskeil@gmail.com>
2024-08-02 11:43:42 +0200

    add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	resolve: {},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		include: ["**/*.{test,spec}.ts"],
+		// globalSetup: "./src/tests/vitest-global.setup.ts",
+		// setupFiles: ["./src/tests/vitest.setup.ts"],
+		coverage: {
+			provider: "istanbul",
+			reporter: ["text", "html"],
+			exclude: [
+				...configDefaults.exclude,
+				"*.{js,mjs}",
+				"**/generated/**",
+				"**/bin/**",
+				"**/interfaces/**",
+				"**/mocks/**",
+				"**/test/**",
+				"**/*.{spec,test,d}.ts",
+			],
+			all: true,
+			clean: true,
+		},
+		snapshotFormat: {
+			escapeString: false,
+		},
+	},
+});
